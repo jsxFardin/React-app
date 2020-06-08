4498b1b9d075d166cf04c025c5c0d2623226cab8
Fardin Ahsan <fardin.ahsan75@gmail.com>
2020-06-09 01:49:56 +0600

    class component

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+/**
+ * class Compoent 
+ */
+
+import React, { Component } from 'react';
+
+class About extends Component {
+
+    render() {
+        return <span>Web developer</span>;
+    }
+}
+
+export default About;
