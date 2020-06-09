b37d0657fff9f2b43da60bce5873eb3f8f86ddfd
Fardin Ahsan <fardin.ahsan75@gmail.com>
2020-06-10 01:20:40 +0600

    hook counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Name from './components/Name';
-import About from './components/About'
-import Hello from './components/Hello'
+// import Name from './components/Name';
+// import About from './components/About'
+// import Hello from './components/Hello';
+import HookCounter from './components/HookCounter'
 
 function App() {
   return (
     <div className="App">
-      {/* <Name/> */}
+      {/* <Name/>
       <About title="Web developer"/>
       <Hello name="Fardin">
         <span>Software Developer</span>
-      </Hello>
+      </Hello> */}
+      <HookCounter/>
     </div>
   );
 }
